bc395f212a0c3d805e251a6232a942dccd59d456
amakyu <amakyu@gmail.com>
2021-09-09 19:22:16 -0700

    add problems into 9-9-2021

--- 9-9-2021-practice.js
+++ 9-9-2021-practice.js
@@ -1,1 +1,25 @@
-asdfadf 
+// Write a `String.prototype.symmetricSubstrings` method that returns an array
+// of substrings which are palindromes in alphabetical order. Only include
+// substrings of length > 1.
+// e.g. "cool".symmetricSubstrings() => ["oo"]
+
+String.prototype.symmetricSubstrings = function () {};
+
+// Write a function, `anagrams(str1, str2)`, that takes in two words and returns
+// a boolean indicating whether or not the words are anagrams. Anagrams are
+// words that contain the same characters but not necessarily in the same order.
+// Solve this without using Array.prototype.sort.
+//
+// Examples:
+// anagrams('listen', 'silent') => true
+// anagrams('listen', 'potato') => false
+//
+
+function anagrams(str1, str2) {}
+
+// Write a function `titleize(str)` that capitalizes each word in a string like
+// a book title.
+// Do not capitalize the following words (unless they are the first word in the
+// string): ["a", "and", "of", "over", "the"]
+
+function titleize(str) {}
